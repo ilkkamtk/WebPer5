0b6f1d329a81e4598acadb41a1ab0c9e6c312b5c
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2017-11-12 20:52:22 +0200

    update

--- js/main_c.js
+++ js/main_c.js
@@ -1,30 +1,30 @@
-var openButtons = document.querySelectorAll('.open-modal');
-var closeButtons = document.querySelectorAll('.close-modal');
+const openButtons = document.querySelectorAll('.open-modal');
+const closeButtons = document.querySelectorAll('.close-modal');
 
-var openModal = function(evt) {
-    evt.preventDefault();
-    console.log(this.href);
-    fetch(this.href).then(function(response) {
-        return response.json();
-    }).then(function(json) {
-        document.querySelector('.modal-content').innerHTML = '<img src="' + json.image + '"><p>' + json.text + '</p>';
-        document.querySelector('.modal').classList.remove('hide');
-        document.querySelector('.modal').classList.add('show');
-    });
-}
+const openModal = (evt) => {
+  evt.preventDefault();
+  console.log(this.href);
+  fetch(this.href).then((response) => {
+    return response.json();
+  }).then((json) => {
+    document.querySelector('.modal-content').innerHTML = '<img src="' +
+        json.image + '"><p>' + json.text + '</p>';
+    document.querySelector('.modal').classList.remove('hide');
+    document.querySelector('.modal').classList.add('show');
+  });
+};
 
-var closeModal = function(evt) {
-    evt.preventDefault();
-    console.log(this.parentElement);
-    this.parentElement.classList.remove('show');
-    this.parentElement.classList.add('hide');
-}
+const closeModal = (evt) => {
+  evt.preventDefault();
+  console.log(this.parentElement);
+  this.parentElement.classList.replace('show', 'hide');
+};
 
+openButtons.forEach((button) => {
+  button.addEventListener('click', openModal);
+});
 
-for (var i = 0; i < openButtons.length; i++) {
-    openButtons[i].addEventListener('click', openModal);
-}
-
-for (var j = 0; j < closeButtons.length; j++) {
-    closeButtons[j].addEventListener('click', closeModal);
+// vaihtoehto forEachille
+for (let i = 0; i < closeButtons.length; i++) {
+  closeButtons[i].addEventListener('click', closeModal);
 }
