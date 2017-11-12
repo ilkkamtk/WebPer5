d8fb587367a8d31ab9b54f526f14d84daf32f6fb
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2017-11-12 20:47:13 +0200

    update

--- js/main_b.js
+++ js/main_b.js
@@ -1,21 +1,23 @@
-var message = document.querySelector('#message');
+'use strict';
 
-var upload = function(evt) {
+const message = document.querySelector('#message');
+
+const upload = (evt) => {
     evt.preventDefault();
     message.innerHTML = 'Upload in progress...';
-    var input = document.querySelector('input[type="file"]');
+    const input = document.querySelector('input[type="file"]');
 
-    var data = new FormData();
+    const data = new FormData();
     data.append('fileToUpload', input.files[0]);
 
     fetch('upload.php', {
         method: 'POST',
         body: data
-    }).then(function(response) {
+    }).then((response) => {
         return response.text();
-    }).then(function(text) {
+    }).then((text) => {
         message.innerHTML = text;
     });
-}
+};
 
 document.querySelector('form').addEventListener('submit', upload);
